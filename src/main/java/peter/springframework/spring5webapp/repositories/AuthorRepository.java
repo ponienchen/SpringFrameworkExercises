16293a2e50e4c7552bb192ba7870add66dff516b
ponienchen <Wifehoney1>
2018-12-31 20:38:14 -0800

    master
    Added docstrings to emphasize that when you extend a Java
    Repository Interface, the Spring Data JPA will provides the
    implementation at runtime.

--- src/main/java/peter/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/peter/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -3,6 +3,9 @@ package peter.springframework.spring5webapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import peter.springframework.spring5webapp.model.book.Author;
 
+//Spring Data Repositories (JPA)
+// - Provides an implementation of the repository pattern
+// - You extend a Java Repository Interface, and Spring Data JPA will provides the implementation AT runtime.
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 
 
